397dfa96fe291cdbf698a88f734cf84f741e4542
Arva Kagdi <arvamkagdi@gmail.com>
2020-12-12 13:01:46 -0500

    Refactoring the code to use chain route handler using express to remove redunduncy and typos

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');   //setting view engine to use ejs
 app.use(bodyParser.urlencoded({extended: true}))  //use body parser to parse our request
 app.use(express.static("public"));    // use public directry to store our static files ex. css files
 
-mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser:true});   //setup connection to mongoDB location
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser:true,  useUnifiedTopology: true});   //setup connection to mongoDB location
 
 const articleSchema = mongoose.Schema({  //create Schema
     title: String,
@@ -19,48 +19,48 @@ const articleSchema = mongoose.Schema({  //create Schema
 
 const Article = mongoose.model("Article",articleSchema)   //create Model
 
+// Refactoring the code to use chain route handler using express to remove redunduncy and typos
 
-// -------- When a client makes a GET request -------//
-app.get("/articles", function(req,res){
-    Article.find({},function(err,foundArticles){
-        if(!err){
-            res.send(foundArticles);
-        }else{
-            res.send(err);
-        }
-    });
-});
-
-
-// -------- When a client makes a POST request -------//
-app.post("/articles", function (req,res) {    // data sent to localhost:3000/articles through postman 
-
-    const newArticle = new Article({    // save the sent data to dabase
-        title : req.body.title,
-        content: req.body.content
+app.route("/articles")
+    // -------- When a client makes a GET request -------//
+    .get(function(req,res){
+        Article.find({},function(err,foundArticles){
+            if(!err){
+                res.send(foundArticles);
+            }else{
+                res.send(err);
+            }
+        });
     })
-    newArticle.save(function(err){        // in save add a callback function to know if data was saved successfully or not 
-        if (!err){
-            res.send("Article added successfully!");
-        }
-        else{
-            res.send(err);
-        }
-    });
-});
 
+    // -------- When a client makes a POST request -------//
+    .post(function (req,res) {    // data sent to localhost:3000/articles through postman 
 
-// -------- When a client makes a DELETE request -------//
-app.delete("/articles",function(req,res){
-    Article.deleteMany(function(err){             // delete everything from the collection
-        if (!err){
-            res.send("Articles successfully deleted");
-        }
-        else{
-            res.send(err);
-        }
+        const newArticle = new Article({    // save the sent data to dabase
+            title : req.body.title,
+            content: req.body.content
+        })
+        newArticle.save(function(err){        // in save add a callback function to know if data was saved successfully or not 
+            if (!err){
+                res.send("Article added successfully!");
+            }
+            else{
+                res.send(err);
+            }
+        });
     })
-});
+
+    // -------- When a client makes a DELETE request -------//
+    .delete(function(req,res){
+        Article.deleteMany(function(err){             // delete everything from the collection
+            if (!err){
+                res.send("Articles successfully deleted");
+            }
+            else{
+                res.send(err);
+            }
+        })
+    });
 
 
 app.listen(3000,function(){
